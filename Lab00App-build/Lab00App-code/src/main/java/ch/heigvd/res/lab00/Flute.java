931dfafd3fcc19acf5e59eb6dc1e2e7388743b46
Sébastien Richoz <sebrichoz@gmail.com>
2016-02-26 08:43:14 +0100

    Update Flute.java

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.res.lab00;
 
 /**
  *
- * @author sebri
+ * @author Sébastien Richoz
  */
 public class Flute implements IInstrument {
     public String play() {
