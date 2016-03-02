d298cc8a7be40650b3e35a83f3065807d7358108
vagrant <vagrant@RES>
2016-03-02 13:30:17 +0000

    Implementation of Orchestra class and modification of Flute sound

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -6,7 +6,7 @@ package ch.heigvd.res.lab00;
  */
 public class Flute implements IInstrument {
     public String play() {
-        return "fufufu";
+        return "trilili";
     }
     public String getColor() {
         return "white";
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Orchestra.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.lab00;
+
+import java.util.*; // Pour avoir les listess
+
+/**
+ *
+ * @author Sébastien Richoz
+ */
+public class Orchestra {
+    
+    List<IInstrument> instruments = new LinkedList<>();
+    
+    public int getNumberOfInstruments() {
+        return instruments.size();
+    }
+    
+    public void addInstrument(IInstrument instru) {
+        instruments.add(instru);
+    }
+    
+    public List<String> makeMusic() {
+        List<String> instrumentSounds = new LinkedList<>();
+        for(IInstrument u : instruments)
+            instrumentSounds.add(u.play());
+        return instrumentSounds;
+    }
+}
